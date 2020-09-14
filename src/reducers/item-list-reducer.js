2577dad4ecf7f739e5938d2233cb59c9040b0c77
Fatma <fceyda@gmail.com>
2020-09-14 13:41:45 -0700

    pass second test

--- src/reducers/item-list-reducer.js
+++ src/reducers/item-list-reducer.js
@@ -1,3 +1,16 @@
 export default (state={}, action) => {
-  return state;
+  const { name, quantity, description, id } = action;
+  switch (action.type) {
+    case "ADD_ITEM":
+      return Object.assign({}, state, {
+        [id] : {
+          name: name,
+          quantity: quantity,
+          description: description,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 }
