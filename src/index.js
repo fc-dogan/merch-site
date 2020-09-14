212e91ad67e61901ca56dca5c147337a778aa4b1
Fatma <fceyda@gmail.com>
2020-09-14 13:55:16 -0700

    create the redux store into index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers/item-list-reducer';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+) 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
+);
+
+store.subscribe(() =>
+  console.log(store.getState())
 );
 
 // If you want your app to work offline and load faster, you can change
